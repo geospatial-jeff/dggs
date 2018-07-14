6cac540148fa2bced9ebaf93626a10690395cc1d
Jeff Albrecht <jeff@Jeffs-MacBook-Pro.local>
2018-07-13 20:25:56 -0700

    Method to build a DGGS of evenly spaced points

--- dggs/core.py
+++ dggs/core.py
@@ -13,3 +13,23 @@ class DGGS():
         self.config = config
         self.h_spacing, self.v_spacing = config.spacing
         self.cols, self.rows = self.config.dimensions
+
+    def PointGrid(self):
+        columns, rows = self.config.dimensions
+
+        centroid_list = []
+
+        for col in range(columns):
+            for row in range(rows):
+                x = self.config.extent[0] + (col * self.h_spacing - col * self.config.h_overlap)
+                y = self.config.extent[3] - (row * self.v_spacing - row * self.config.v_overlap)
+                centroid_list.append([x,y])
+        return PointDGGS(centroid_list, self.config.epsg)
+
+class PointDGGS():
+
+    """Class containing functionality for Point DGGS"""
+
+    def __init__(self, centroid_list, epsg):
+        self.centroids = centroid_list
+        self.epsg = epsg
