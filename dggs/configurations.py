d6a5038e747af71d5f1f1665495655aa052e4303
Jeff Albrecht <jeff@Jeffs-MacBook-Pro.local>
2018-07-13 20:06:21 -0700

    DGGS configuration where grid size is specified in number of pixels of given spatial resolution

--- dggs/configurations.py
+++ dggs/configurations.py
@@ -1,3 +1,4 @@
+import math
 
 class DGGSConfiguration():
 
@@ -16,3 +17,24 @@ class DGGSConfiguration():
     @property
     def height(self):
         return float(self.extent[3] - self.extent[2])
+
+class PixelConfiguration(DGGSConfiguration):
+
+    """Class for configuring DGGS where grid size is specified in number of pixels"""
+
+    def __init__(self, extent, h_spacing, v_spacing, epsg, xres, yres, h_overlap=0, v_overlap=0):
+        DGGSConfiguration.__init__(self, extent, h_spacing, v_spacing, epsg, h_overlap, v_overlap)
+        self.xres = xres
+        self.yres = yres
+
+    @property
+    def dimensions(self):
+        step_x = float(self.h_spacing * self.xres)
+        step_y = float(self.v_spacing * self.yres)
+        rows = int(round(self.width) / round(step_y))
+        cols = int(round(self.height) / round(step_x))
+        return (cols, rows)
+
+    @property
+    def spacing(self):
+        return (float(self.h_spacing * self.xres), float(self.v_spacing * self.yres))
